8c8687cd5b49bf57e78263ed7430208774ffd031
Antony D'Amico <adamico@hispatecanalytics.com>
2021-05-07 16:12:33 +0200

    Basic hero component

---
+++ src/components/common/Hero.vue
@@ -0,0 +1,31 @@
+<template>
+<section class="hero is-link">
+    <div class="hero-body">
+      <p class="title">
+        {{ title }}
+      </p>
+      <p class="subtitle">
+        {{ subtitle }}
+      </p>
+      <slot/>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'Hero',
+  props: {
+    title: { type: String, default: '' },
+    subtitle: { type: String, default: '' },
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.hero {
+  margin-bottom: 5rem;
+}
+</style>
