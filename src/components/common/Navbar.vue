29835157b1e81bf7759f8e025928155af300e4c2
Antony D'Amico <adamico@hispatecanalytics.com>
2021-05-07 16:14:53 +0200

    Creating basic navbar

---
+++ src/components/common/Navbar.vue
@@ -0,0 +1,30 @@
+<template>
+  <b-navbar fixed-top spaced>
+    <template #brand>
+      <router-link to="/">
+        <b-button type="is-dark">Home</b-button>
+      </router-link>
+    </template>
+
+    <template #end>
+      <div class="buttons">
+        <b-button type="is-primary is-light" @click="login">Iniciar Sesión</b-button>
+        <b-button type="is-primary" @click="signUp">Registro</b-button>
+      </div>
+    </template>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+  methods: {
+    login() {
+      //
+    },
+
+    signUp() {
+      //
+    },
+  },
+};
+</script>
