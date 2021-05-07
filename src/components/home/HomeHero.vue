b6039029b0149b472b8c65ddf0373e13512ec41d
Antony D'Amico <adamico@hispatecanalytics.com>
2021-05-07 21:52:44 +0200

    Hidding new article button when no authenticated

--- src/components/home/HomeHero.vue
+++ src/components/home/HomeHero.vue
@@ -4,15 +4,21 @@
       title="Blogging"
       subtitle="Plataforma de artículos"
     >
-      <b-button size="is-large">Nuevo Artículo</b-button>
+      <b-button v-if="isAuthenticated" size="is-large">
+        Nuevo Artículo
+      </b-button>
     </ad-hero>
   </div>
 </template>
 
 <script>
 import AdHero from '@/components/common/Hero.vue';
+import { mapGetters } from 'vuex';
 
 export default {
   components: { AdHero },
+  computed: {
+    ...mapGetters('Auth', ['isAuthenticated']),
+  },
 };
 </script>
