41ab4119013482435d0f8828dab239c943e46061
Antony D'Amico <adamico@hispatecanalytics.com>
2021-05-07 19:31:00 +0200

    Adding trailing slash check

--- src/api/service.js
+++ src/api/service.js
@@ -5,10 +5,18 @@ export const axiosClient = axios.create({
   baseURL: BASE_URL,
 });
 
+axiosClient.interceptors.request.use((config) => {
+  if (config.url.substr(-1) !== '/') {
+    // eslint-disable-next-line no-param-reassign
+    config.url += '/';
+  }
+  return config;
+});
+
 export const ArticleService = {
   baseString: 'articles',
 
-  all() {
-    return axiosClient.get(this.baseString);
+  all(sortOption) {
+    return axiosClient.get(this.baseString, { params: { sort: sortOption } });
   },
 };
