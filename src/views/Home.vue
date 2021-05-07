c7113cb1ec1391530ade39ab63391de66fdd4ac2
Antony D'Amico <adamico@hispatecanalytics.com>
2021-05-07 18:20:14 +0200

    Using sort row

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="home">
     <ad-home-hero />
+    <ad-sort-row />
     <ad-article-list></ad-article-list>
   </div>
 </template>
 
 <script>
 import AdHomeHero from '@/components/home/HomeHero.vue';
+import AdSortRow from '@/components/home/SortRow.vue';
 import AdArticleList from '@/components/articles/ArticleList.vue';
 
 export default {
   name: 'Home',
-  components: { AdHomeHero, AdArticleList },
+  components: { AdHomeHero, AdSortRow, AdArticleList },
 };
 </script>
 
