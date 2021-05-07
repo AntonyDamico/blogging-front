b9df678c4d8ad1f7005981f034e6de7a7fd937dc
Antony D'Amico <adamico@hispatecanalytics.com>
2021-05-07 23:24:23 +0200

    Creating article detail components

---
+++ src/components/articles/ArticleBody.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="columns" v-if="article">
+    <img class="column is-half" :src="article.imageSrc">
+    <div class="article-body column is-half ">
+      {{ article.body }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ArticleBody',
+
+  props: {
+    article: { type: Object, default: null },
+  },
+};
+</script>
+
+<style lang="scss" sccoped>
+.columns {
+  padding: 0 10rem;
+
+  .article-body {
+    text-align: justify;
+  }
+}
+</style>
---
+++ src/components/articles/ArticleHero.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <ad-hero
+      :title="title"
+      :subtitle="author"
+    >
+      {{ date }}
+    </ad-hero>
+  </div>
+</template>
+
+<script>
+import AdHero from '@/components/common/Hero.vue';
+
+export default {
+  components: { AdHero },
+  props: {
+    title: { type: String, default: '' },
+    author: { type: String, default: '' },
+    date: { type: String, default: '' },
+  },
+};
+</script>
