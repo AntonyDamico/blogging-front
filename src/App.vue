b5a81a86de1c40193f4335aba7c29ed4c5541054
Antony D'Amico <adamico@hispatecanalytics.com>
2021-05-07 16:18:51 +0200

    Using navbar

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <ad-navbar />
     </div>
     <router-view/>
   </div>
 </template>
+
+<script>
+import AdNavbar from '@/components/common/Navbar.vue';
+
+export default {
+  components: { AdNavbar },
+};
+</script>
 
 <style lang="scss">
 #app {
