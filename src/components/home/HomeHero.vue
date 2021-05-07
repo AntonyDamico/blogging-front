99dd4403eb369edab63efd0803e700c76033ed0f
Antony D'Amico <adamico@hispatecanalytics.com>
2021-05-07 16:13:45 +0200

    creating home hero

---
+++ src/components/home/HomeHero.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <ad-hero
+      title="Blogging"
+      subtitle="Plataforma de artículos"
+    >
+      <b-button size="is-large">Nuevo Artículo</b-button>
+    </ad-hero>
+  </div>
+</template>
+
+<script>
+import AdHero from '@/components/common/Hero.vue';
+
+export default {
+  components: { AdHero },
+};
+</script>
